ca175eef7d5046f092c570467508b8439460f90d
ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-05-20 10:35:27 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+// import cors from 'cors'
+const socket = require('socket.io')(80, {origins: "foo.bar:80"});
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = socket.listen(server);
+const port = process.env.PORT || 3000;
+
+// app.use(cors())
+
+server.listen(port, function () {
+	console.log('Server listening at port %d', port);
+});
+
+io.on('connection', function (socket) {
+	socket.on("join", param => {
+		console.log("user join")
+	})
+});
