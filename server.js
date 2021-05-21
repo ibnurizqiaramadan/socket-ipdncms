14598596d09cdb0789040f2ba38feea6ff78ec35
ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-05-21 13:12:39 +0700

    update

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-const io = require("socket.io")(6996,{
+var express = require('express')
+var app = express()
+var http = require('http').createServer(app);
+
+http.listen(process.env.PORT || 6996, function() {
+  var host = http.address().address
+  var port = http.address().port
+  console.log('App listening at http://%s:%s', host, port)
+});
+
+const io = require("socket.io")(http,{
     cors: {
         origin: ["http://localhost", "http://192.168.1.25", "http://ipdncms.test", "https://ipdncms.xyrus10.com", "http://ipdncms.xyrus10.com"],
         methods: ["GET", "POST"],
